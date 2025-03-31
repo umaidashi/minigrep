42792bce3b2b6c0c12dd44612a450ff56924a297
umaidashi <umaidashi18@gmail.com>
2025-03-31 14:12:16 +0900

    feat: not use extern crate

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,8 @@
-extern crate minigrep;
+// extern crate minigrep;
+use minigrep::{run, Config};
 
 use std::env;
 use std::process;
-
-use minigrep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -16,7 +15,7 @@ fn main() {
     // println!("Searching for {}", config.query);
     // println!("In file {}", config.filename);
 
-    if let Err(e) = minigrep::run(config) {
+    if let Err(e) = run(config) {
         eprintln!("Application error: {}", e);
         process::exit(1);
     }
